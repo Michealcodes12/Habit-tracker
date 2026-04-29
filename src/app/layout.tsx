c27dc65f63af7da4cb8f3614219fc909e856f032
michealcodes12 <saintkeys1224@gmail.com>
2026-04-29 23:50:08 +0100

    fix: ions not diaplaying proplery

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     >
       <head>
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" precedence="default" />
       </head>
       <body className="min-h-full flex flex-col bg-background text-on-background font-sans">
         {children}
